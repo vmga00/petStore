c0e9d86d3fd516bec2500ad85d58e23413ce3a38
VictorGonzalez <vmga00@gmail.com>
2021-06-28 21:43:57 +0200

    created User endpoints and class
    Signed-off-by: VictorGonzalez <vmga00@gmail.com>

--- src/main/java/com/petstore/api/constants/EndPoints.java
+++ src/main/java/com/petstore/api/constants/EndPoints.java
@@ -1,4 +1,7 @@
 package com.petstore.api.constants;
 
 public class EndPoints {
+    public static final String GET_ORDER_BY_ID = "/store/order/";
+    public static final String SET_ORDER = "/store/order";
+    public static final String DELETE_ORDER = "/store/order/";
 }
--- src/main/java/com/petstore/api/models/User.java
+++ src/main/java/com/petstore/api/models/User.java
@@ -1,4 +1,166 @@
 package com.petstore.api.models;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "username",
+        "firstName",
+        "lastName",
+        "email",
+        "password",
+        "phone",
+        "userStatus"
+})
+@Generated("jsonschema2pojo")
 public class User {
+
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("username")
+    private String username;
+    @JsonProperty("firstName")
+    private String firstName;
+    @JsonProperty("lastName")
+    private String lastName;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
+    @JsonProperty("phone")
+    private String phone;
+    @JsonProperty("userStatus")
+    private Integer userStatus;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public User() {
+    }
+
+    /**
+     *
+     * @param firstName
+     * @param lastName
+     * @param password
+     * @param userStatus
+     * @param phone
+     * @param id
+     * @param email
+     * @param username
+     */
+    public User(Integer id, String username, String firstName, String lastName, String email, String password, String phone, Integer userStatus) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userStatus = userStatus;
+    }
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("username")
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonProperty("username")
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @JsonProperty("firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @JsonProperty("firstName")
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @JsonProperty("lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    @JsonProperty("lastName")
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    @JsonProperty("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("password")
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty("password")
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @JsonProperty("phone")
+    public String getPhone() {
+        return phone;
+    }
+
+    @JsonProperty("phone")
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @JsonProperty("userStatus")
+    public Integer getUserStatus() {
+        return userStatus;
+    }
+
+    @JsonProperty("userStatus")
+    public void setUserStatus(Integer userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
