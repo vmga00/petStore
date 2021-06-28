87ab524f6c86a633d26ef12d0dd9ffb2dec844db
VictorGonzalez <vmga00@gmail.com>
2021-06-28 21:49:12 +0200

    added the store endpoitns and the order class
    Signed-off-by: VictorGonzalez <vmga00@gmail.com>

--- src/main/java/com/petstore/api/constants/EndPoints.java
+++ src/main/java/com/petstore/api/constants/EndPoints.java
@@ -5,4 +5,9 @@ public class EndPoints {
     public static final String GET_USER_BY_USERNAME = "/user/";
     public static final String USER_LOGIN = "/user/login?";
     public static final String USER_LOGOUT = "/user/logout";
+
+    public static final String GET_ORDER_BY_ID = "/store/order/";
+    public static final String SET_ORDER = "/store/order";
+    public static final String DELETE_ORDER = "/store/order/";
+
 }
