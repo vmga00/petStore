125aab0563bc3e36e6e3dc817a2efb5e169119f3
VictorGonzalez <vmga00@gmail.com>
2021-06-28 21:59:48 +0200

    added extra classes related to pets
    Signed-off-by: VictorGonzalez <vmga00@gmail.com>

--- src/main/java/com/petstore/api/models/Pet.java
+++ src/main/java/com/petstore/api/models/Pet.java
@@ -1,4 +1,137 @@
 package com.petstore.api.models;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "name",
+        "category",
+        "photoUrls",
+        "tags",
+        "status"
+})
+@Generated("jsonschema2pojo")
 public class Pet {
+
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("category")
+    private Category category;
+    @JsonProperty("photoUrls")
+    private List<String> photoUrls = null;
+    @JsonProperty("tags")
+    private List<Tag> tags = null;
+    @JsonProperty("status")
+    private String status;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Pet() {
+    }
+
+    /**
+     *
+     * @param photoUrls
+     * @param name
+     * @param id
+     * @param category
+     * @param tags
+     * @param status
+     */
+    public Pet(Integer id, String name, Category category, List<String> photoUrls, List<Tag> tags, String status) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.photoUrls = photoUrls;
+        this.tags = tags;
+        this.status = status;
+    }
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("category")
+    public Category getCategory() {
+        return category;
+    }
+
+    @JsonProperty("category")
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @JsonProperty("photoUrls")
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    @JsonProperty("photoUrls")
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    @JsonProperty("tags")
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    @JsonProperty("tags")
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    @JsonProperty("status")
+    public String getStatus() {
+        return status;
+    }
+
+    @JsonProperty("status")
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
---
+++ src/main/java/com/petstore/api/models/Tag.java
@@ -0,0 +1,72 @@
+package com.petstore.api.models;
+
+import com.fasterxml.jackson.annotation.*;
+
+import javax.annotation.Generated;
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "name"
+})
+@Generated("jsonschema2pojo")
+public class Tag {
+
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("name")
+    private String name;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Tag() {
+    }
+
+    /**
+     *
+     * @param name
+     * @param id
+     */
+    public Tag(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
