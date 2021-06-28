3e6d7f338671207275eba3eda8f5757872eaf3cb
VictorGonzalez <vmga00@gmail.com>
2021-06-28 21:46:52 +0200

    updated endpoints
    Signed-off-by: VictorGonzalez <vmga00@gmail.com>

--- src/main/java/com/petstore/api/constants/EndPoints.java
+++ src/main/java/com/petstore/api/constants/EndPoints.java
@@ -1,7 +1,8 @@
 package com.petstore.api.constants;
 
 public class EndPoints {
-    public static final String GET_ORDER_BY_ID = "/store/order/";
-    public static final String SET_ORDER = "/store/order";
-    public static final String DELETE_ORDER = "/store/order/";
+    public static final String CREATE_USER = "/user";
+    public static final String GET_USER_BY_USERNAME = "/user/";
+    public static final String USER_LOGIN = "/user/login?";
+    public static final String USER_LOGOUT = "/user/logout";
 }
