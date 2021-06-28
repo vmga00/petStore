e8211f95262491534791e8f72ba2e1ec70dad53b
VictorGonzalez <vmga00@gmail.com>
2021-06-28 21:56:27 +0200

    added the test for the store and orders
    Signed-off-by: VictorGonzalez <vmga00@gmail.com>

--- src/main/java/com/petstore/api/helpers/StoreServiceHelpers.java
+++ src/main/java/com/petstore/api/helpers/StoreServiceHelpers.java
@@ -1,4 +1,25 @@
 package com.petstore.api.helpers;
 
-public class StoreServiceHelpers {
+import com.petstore.api.constants.EndPoints;
+import com.petstore.api.models.Order;
+import io.restassured.response.Response;
+
+public class StoreServiceHelpers extends com.petstore.api.utils.Utils {
+
+    public StoreServiceHelpers() {
+        initialize();
+    }
+
+    public Response setOrder(Order order) {
+        return post(EndPoints.SET_ORDER, order);
+    }
+
+    public Response getOrderById(String id) {
+        return get(EndPoints.GET_ORDER_BY_ID, id);
+    }
+
+
+    public Response deleteOrder(String id) {
+        return remove(EndPoints.DELETE_ORDER, id);
+    }
 }
--- src/test/java/com/petstore/api/tests/Test_Store.java
+++ src/test/java/com/petstore/api/tests/Test_Store.java
@@ -1,4 +1,52 @@
 package com.petstore.api.tests;
 
+import com.petstore.api.helpers.StoreServiceHelpers;
+import com.petstore.api.models.Order;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class Test_Store {
+    private StoreServiceHelpers storeServiceHelpers;
+    Order order;
+
+    @BeforeClass
+    private void init() {
+        storeServiceHelpers = new StoreServiceHelpers();
+        order = new Order();
+        order.setId(100);
+        order.setPetId(1000);
+        order.setQuantity(5);
+        order.setShipDate("2021-06-28T16:01:45.616Z");
+        order.setStatus("approved");
+        order.setComplete(true);
+    }
+
+    @Test
+    public void test_store_PLACE_ORDER() {
+        Response result = storeServiceHelpers.setOrder(order);
+        result.prettyPeek();
+    }
+
+    @Test
+    public void test_store_GET_ORDER_BY_ID() {
+        Response result = storeServiceHelpers.getOrderById(order.getId().toString());
+        result.prettyPeek();
+    }
+
+    @Test
+    public void test_store_DELETE_ORDER() {
+        Response result = storeServiceHelpers.getOrderById(order.getId().toString());
+        if (result.getStatusCode() == 200) {
+            System.out.println("Order found, deleting...");
+            result = storeServiceHelpers.deleteOrder(order.getId().toString());
+        } else {
+            System.out.println("Order not found, creating order...");
+            storeServiceHelpers.setOrder(order);
+            System.out.println("Order created, attempting to delete it...");
+            storeServiceHelpers.deleteOrder(order.getId().toString());
+            storeServiceHelpers.getOrderById(order.getId().toString());
+        }
+        result.prettyPeek();
+    }
 }
